7dc55d8d252b83edad9337dc5619d813ca686660
Murat Bastas <muratbsts@gmail.com>
2020-04-12 16:34:03 +0300

    Acts as list and ancestry integration for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 class Category < ApplicationRecord
+  has_ancestry
+  acts_as_list scope: [:ancestry]
+
   has_many :products, dependent: :nullify
 
   validates :name, presence: true
